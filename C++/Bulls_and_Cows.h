52a1b61ed39d3b0be8cb2a9ce117b33ff32add92
4yejw <eo4yejw@naver.com>
2023-08-28 23:15:49 +0900

    commit

--- C++/Bulls_and_Cows.h
+++ C++/Bulls_and_Cows.h
@@ -14,7 +14,7 @@ public:
 	void ShowGameResult();//각 게임의 결과를 출력
 	void GuessAnswer();//정답을 추론
 	void SetSBO(int a, int b, int c, int d);//SBO 계산
-	void ShowTotalResult();//지금까지 문제들의 총 결과,시도횟수,점수,총점수,평균점수를 출력
+	void ShowTotalResult();//다섯 문제의 총 점수,평균점수를 출력
 
 private:
 	int game_number;//몇 번째 문제
